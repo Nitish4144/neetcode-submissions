6f24292fd34a7ccdedda393637d2c4780fcbeb5e
Nitish <devadanitish@gmail.com>
2026-07-22 18:57:18 +0530

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string,vector<string>> mp;
+        unordered_map<string,bool> vis;
+        int w = beginWord.size();
+        bool f =false;
+
+        for(auto& word:wordList){
+            vis[word] = false;
+            if( word == endWord) f = true;
+            for(int j=0;j<w;j++){
+                string s = word.substr(0,j)+"*"+word.substr(j+1,w-j-1);
+                mp[s].push_back(word);
+            }
+        }
+        if(!f) return 0;
+
+        deque<string> q;
+        q.push_back(beginWord);
+        int ans =0;
+
+        while(!q.empty()){
+            int sz = q.size();
+            for(int szi =0;szi<sz;szi++){
+                string word = q.front();
+                q.pop_front();
+                for(int j=0;j<w;j++){
+                    string s = word.substr(0,j)+"*"+word.substr(j+1,w-j-1);
+                    bool k = true;
+                    for(auto& wrd:mp[s]){
+                        if(wrd != word and vis[wrd] == false){
+                            // if(k) ans+=1,k=false;
+                            if(wrd == endWord) return ans+2;
+                            q.push_back(wrd);
+                            vis[wrd]=true;
+                        }
+                    }
+                }
+            }
+            ans+=1;
+        }
+        return 0;
+    }
+};
